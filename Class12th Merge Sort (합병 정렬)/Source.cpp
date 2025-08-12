48009a80610574392fe43eea9d15427aa3f7ef7d
gydud0819 <pstar0819@naver.com>
2025-08-12 15:45:33 +0900

    (2025-08-12) 동적 계획법

---
+++ Class12th Merge Sort (합병 정렬)/Source.cpp
@@ -0,0 +1,94 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+#pragma region Merge Sort (병합 정렬)
+// 하나의 리스트를 2개의 균일한 크기로 분할하고
+// 분할된 부분 리스트를 정렬한 다음, 2개의 정렬된 부분 리스트를
+// 합하여 전체가 정렬된 리스트가 되게 하는 방법이다.
+
+// 1. 리스트의 길이가 0 or 1이 되면 이미 정렬된 것으로 간주한다.
+// 2. 그렇지 않은 경우
+// 2-1. 정렬되지 않은 리스트를 반으로 나눈 다음 비슷한 크기의 두 부분 리스트로 나눈다.
+// 2-2. 각 부분 리스트를 재귀적으로 병합 정렬을 이용하여 정렬한다.
+// 2-3. 두 부분 리스트를 다시 하나의 정렬된 리스트로 병합한다.
+
+void Merge(int list[], int start, int mid, int end)
+{
+	// mid = (start + end) / 2		// 이게 피봇인가 
+	int* container = new int[end - start + 1];		//
+	int count = 0;
+	int left = start;		// start? start + 1 ? 둘중 하나 같은디 
+	int right = mid + 1;		// mid + 1? t.....q end? 머고 진짜 
+
+	// while?
+	// 재귀를 안쓴다구요...? ㅏ..
+	// 두 부분의 배열 병합하기
+	while (left <= mid && right <= end)
+	{
+		// 왼쪽 값이 오른쪽값보다 작거나같다? ㅓ <= 어케 해석하더라 '<=' 이게...어케 해석하더라..? 작거나같다..? ???
+		if (list[left] <= list[right])
+		{
+			container[count++] = list[left++];
+		}
+		else
+		{
+			container[count++] = list[right++];
+		}
+
+	}
+
+	// while을 한번더? 어케씀? 3번정도 쓴다고? 
+	// 남은 왼쪽 배열의 요소들 복사하기
+	while (left <= mid)
+	{
+		container[count++] = list[left++];
+	}
+
+	while (right <= end)
+	{
+		container[count++] = list[right++];
+	}
+
+	// 원본 배열에 정렬된 임시 배열의 값 복사하기
+	for (int i = 0; i < end - start + 1; i++)
+	{
+		list[start + i] = container[i];
+	}
+
+	// 해제하기
+	delete[] container;
+}
+
+//  3 5 2 7 4 1 8 6 
+void MergeSort(int list[], int start, int end)
+{
+	// 어케 쪼개요..? 이렇게 쪼개면 되구나 
+	if (start < end)
+	{
+		int mid = (start + end) / 2;
+		MergeSort(list, start, mid);
+		MergeSort(list, mid + 1, end);
+
+		Merge(list, start, mid, end);		// 이렇게 호출하는건가? 
+	}
+
+}
+
+
+#pragma endregion
+
+int main()
+{
+	int list[] = { 3,5,2,7,4,1,8,6 };
+	int size = sizeof(list) / sizeof(list[0]);
+	MergeSort(list, 0, size - 1);
+
+	for (auto& elem : list)
+	{
+		cout << elem << " ";
+	}
+	return 0;
+}
--- Program/Program.cpp
+++ Program/Program.cpp
@@ -1,40 +1,61 @@
 ﻿#include "Util.h"
 
-#pragma region Merge Sort (병합 정렬)
-// 하나의 리스트를 2개의 균일한 크기로 분할하고
-// 분할된 부분 리스트를 정렬한 다음, 2개의 정렬된 부분 리스트를
-// 합하여 전체가 정렬된 리스트가 되게 하는 방법이다.
+#pragma region 동적 계획법 (Dynamic Programming)
+// 특정 범위까지의 값을 구하기 위해 그것과 다른 범위 까지의 값을 이용하여 효율적으로 값을 구하는 알고리즘이다.
 
-// 1. 리스트의 길이가 0 or 1이 되면 이미 정렬된 것으로 간주한다.
-// 2. 그렇지 않은 경우
-// 2-1. 정렬되지 않은 리스트를 반으로 나눈 다음 비슷한 크기의 두 부분 리스트로 나눈다.
-// 2-2. 각 부분 리스트를 재귀적으로 병합 정렬을 이용하여 정렬한다.
-// 2-3. 두 부분 리스트를 다시 하나의 정렬된 리스트로 병합한다.
+// 겹치는 부분 문제 (Overlapping Subproblems)
+// 동일한 작은 문제들이 반복하여 나타나는 경우를 의미한다.
 
-//  3 5 2 7 4 1 8 6 
-void MergeSort(int list[], int start, int end)
-{
-	// 어케 쪼개요..? 이렇게 쪼개면 되구나 
-	if (start < end)
-	{
-		int mid = (start + end) / 2;
-		MergeSort(list, start, mid);
-		MergeSort(list, mid + 1, end);
-	}
+// 최적 부분 구조 (Optimal Substructure)
+// 부분 문제의 최적 결과 값을 사용하여 전체 문제의 최적의 결과를 낼 수 있는 경우를 의미한다. 
 
-}
-
-void Merge(int list[], int mid, int end)
-{
-	// 
-	int count = 0;
-}
+// 메모이재이션 (Memoization)
+// 프로그램이 동일한 계산을 반복해야 할 때, 이전에 계산한 값을 메모리에 저장함으로써 
+// 동일한 계산을 반복 수행하는 작업을 제거하여 프로그램이 실행 속도를 향상시키는 방법.
 #pragma endregion
+
+// 1 1 2 3 5 8 13 21 34 55 89 144 233 377 610 980 1590 2570...
+int fibonacci(int n)
+{
+	// 탙ㄹ출조건?
+	// 이게 탈출조건..?
+	if (n == 0)
+		return 0;
+	if (n == 1)
+		return 1;
+	return fibonacci(n - 1) + fibonacci(n - 2);		// <- 점화식				 리턴하겟군
+}
+
+// 동적 계획법으로하는 피보나치 왜 이따구로 풀었지? 
+int fibonacci(int n, int list[])
+{
+	list[0] = 0;
+	list[1] = 1;
+	
+	for (int i = 2; i <= n; i++)
+	{
+		list[i] = list[i - 1] + list[i - 2];
+	}
+	return list[n];
+
+}
+
+int fibonacci(int n, int list[])
+{
+	if (list[n] <= 1)
+		return list[n] + 1;
+
+
+}
 
 int main()
 {
-	int list[] = { 3,5,2,7,4,1,8,6 };
-	int size = sizeof(list) / sizeof(list[0]);
-	MergeSort(list, 0, size - 1);
+	// 출력이..없어서 그런가? 
+	//cout << fibonacci(45);
+
+	// 내가 출력을 못하나? 
+	int list[10001];
+	cout << fibonacci(100, list);
+
 	return 0;
 }
